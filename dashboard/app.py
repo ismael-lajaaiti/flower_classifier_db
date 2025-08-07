4239a0ebc7e33376a1ca6258164b8fe5e1dac4ab
Ismaël Lajaaiti <ismael.lajaaiti@gmail.com>
2025-08-07 15:50:09 +0200

    Starting dashboard rework

--- dashboard/app.py
+++ dashboard/app.py
@@ -1,59 +1,62 @@
 import streamlit as st
 import pandas as pd
+import plotly.graph_objects as go
 import duckdb
 import sys
 import os
 
-# Add the src/ folder to Python's import path
+# Add the src/ folder to Python's import path -- for deployment.
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "src")))
 
 DB_PATH = "data/flowers.db"
 
-st.title("Flower classifier")
+st.title("Performance of the flower classifier")
 
-# Join predictions and images and compute misclassifications
+st.write(
+    """
+    The goal of the neural network presented therein is to classify correctly flower images.
+    As this project is intended to be small, we focus on a rather small dataset that contain
+    only 6 different flower species. 
+    Choosing a small dataset and a simple task allows us to focus on the tools (packages, etc.) rather
+    idiosyncratic details of a given dataset.
+
+    Our classifier is based on a [ResNet 18](https://pytorch.org/hub/pytorch_vision_resnet/) trained on the 
+    [tf_flower](https://www.tensorflow.org/datasets/catalog/tf_flowers) dataset. 
+
+    Below are the training logs of the model.
+    """
+)
+
 query = f"""
 SELECT 
-    *,
-    CASE WHEN p.true_label != p.predicted_label THEN 1 ELSE 0 END AS is_misclassified
-FROM sqlite_scan('{DB_PATH}', 'predictions') p
-JOIN sqlite_scan('{DB_PATH}', 'images') i
-ON p.image_id = i.id
+    epoch,
+    train_loss AS training,
+    val_loss AS validation
+    FROM sqlite_scan('{DB_PATH}', 'training_logs')
 """
 
 df = duckdb.sql(query).df()
 
-# Overall misclassification rate.
-overall_mis_rate = df["is_misclassified"].mean() * 100
-st.metric("Overall Misclassification Rate", f"{overall_mis_rate:.2f}%")
-
-# Select a flower.
-labels = sorted(df["true_label"].unique())
-selected_label = st.selectbox("Choose a flower class", labels)
-
-# False negatives (true = selected, predicted != selected).
-true_class_df = df[df["true_label"] == selected_label]
-fn_count = (true_class_df["predicted_label"] != selected_label).sum()
-fn_rate = fn_count / len(true_class_df) * 100
-
-# False positives (true != selected, predicted = selected).
-pred_class_df = df[df["predicted_label"] == selected_label]
-fp_count = (pred_class_df["true_label"] != selected_label).sum()
-fp_rate = fp_count / len(pred_class_df) * 100
-
-
-st.subheader(f"Model performance on *{selected_label}*")
-
-col1, col2 = st.columns(2)
-col1.metric(
-    "False Negative Rate",
-    f"{fn_rate:.2f}%",
-    delta=f"{fn_rate - overall_mis_rate:+.2f}%",
-    delta_color="inverse",
+# Create the plot.
+fig = go.Figure()  # Empty canva.
+fig.add_trace(
+    go.Scatter(
+        x=df["epoch"], y=df["training"], mode="lines+markers", name="Training Loss"
+    )
 )
-col2.metric(
-    "False Positive Rate",
-    f"{fp_rate:.2f}%",
-    delta=f"{fp_rate - overall_mis_rate:+.2f}%",
-    delta_color="inverse",
+fig.add_trace(
+    go.Scatter(
+        x=df["epoch"], y=df["validation"], mode="lines+markers", name="Validation Loss"
+    )
 )
+fig.update_layout(
+    xaxis_title="Epoch",
+    yaxis_title="Loss",
+    legend=dict(
+        x=0.80,
+        y=0.98,
+    ),
+    margin=dict(l=40, r=40, t=40, b=40),
+    height=400,
+)
+st.plotly_chart(fig)
